0916a17ef026ebd556f9dd4e6dd69f55ceac04cb
指紋_the_flemmard42/22 <simon.beilin2007@gmail.com>
2026-04-04 17:33:17 +0200

    player+coordonnées_de_cases_non_finalisé

---
+++ class_Player.py
@@ -0,0 +1,30 @@
+
+
+
+class Player:
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+        if self.color== 0:
+            self.color =(255,255,255)
+    def askMove(self):
+        ask_user=input("Votre prochain mouvement ?")
+
+        if ask_user == 0:
+            return "1"
+        else:
+            return ask_user,player_one.askMove()
+
+
+coo=[]
+#Génère une liste de A1 à H8
+for i in range(1,9):
+    coo.append(f'A{i}'),coo.append(f'B{i}'),coo.append(f'C{i}'),coo.append(f'D{i}'),coo.append(f'E{i}'),coo.append(f'F{i}'),coo.append(f'G{i}'),coo.append(f'H{i}')
+print(coo)
+print(len(coo))
+player_one=Player('Simon',0)
+print(player_one.askMove())
+
+
+#l’emplacement de destination. Par exemple, si on reprend l’état initial de l’échiquier, alors les coups
+#suivants sont tous valides : « Nb1 Nc3 », « Pd1 Pd3 », « Pc1 Pc4 »
