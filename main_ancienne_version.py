0cebc111c1a35bdfebf789bf7c88c317c5c9d42c
指紋_the_flemmard42/22 <simon.beilin2007@gmail.com>
2026-04-04 15:32:52 +0200

    version_non_optimisée

--- main_ancienne_version.py
+++ main_ancienne_version.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import sys
-longueur= 800
-largeur = 800
+longueur= 400
+largeur = 400
 cote_l = longueur/8
 width_rect = (cote_l)/2
 cl= int(width_rect)
@@ -9,32 +9,37 @@ screen = pg.display.set_mode((longueur, largeur))
 clock = pg.time.Clock()
 background= pg.draw.rect(screen,(128,128,128),(0,0,longueur,largeur))
 clock.tick(60)
-l = 100
-for i in range(0,65):
-    m=100*i
+l = longueur/8
+# Plateu généré mais par optimisé (Pas de proportionnalité avec la fenêtre)
+for i in range(0,8):
+    m=l*i
     if i%2==0:
         pg.draw.rect(screen, (255, 255, 255), (m, 0, cote_l, cote_l), cl)
         pg.draw.rect(screen, (0, 0, 0), (m, l, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 100, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 150, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 200, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 250, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 300, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 350, cote_l, cote_l), cl)
+
     else:
         pg.draw.rect(screen, (0, 0, 0), (m, 0, cote_l,cote_l), cl)
         pg.draw.rect(screen, (255, 255, 255), (m, l, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 100, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 150, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 200, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 250, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (0, 0, 0), (m, 300, cote_l, cote_l), cl)
+        pg.draw.rect(screen, (255, 255, 255), (m, 350, cote_l, cote_l), cl)
+
+
+
 
 #pg.draw.rect(screen,(255,255,255),(0,0,cote_l,cote_l),cl)
 #pg.draw.rect(screen,(0,0,0),(cote_l,0,cote_l,cote_l),cl)
 #pg.draw.rect(screen,(255,255,255),(cote_l*2,0,cote_l,cote_l),cl)
 #pg.draw.rect(screen,(255,255,255),(cote_l*3,0,cote_l,cote_l),cl)
-class Piece:
-    def __init__(self,x,y,point,color,image):
-        self.x=x
-        self.y=y
-        self.point=point
-        self.color=color
-        self.image=image
-        def action (self):
-           pass
-class Pion(Piece):
-     def action(self):
-        return 'kk'
 #im= pg.image.load('sources\Pb.png').convert()
 #im= pg.image.load('playertag.bmp').convert().set_colorkey('black')
 #piece= Piece(0,0,1,None,im)
