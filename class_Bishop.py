73364c99f6aa3e2a6e1533c594ae5799db4fd45b
Maxence7595 <Maxence.amirkhanian@hotmail.com>
2026-04-18 09:54:55 +0200

    Add move calculation for Bishop piece
    Implement action method for Bishop piece to calculate valid moves.

--- class_Bishop.py
+++ class_Bishop.py
@@ -2,5 +2,30 @@ import class_test.py
 import pygame as pg
 import os
 class Bishop(Piece):
-    def action(self):
-       pass
+    def action(self, board, x, y):
+        moves = []
+      directions = [
+            (1, 1),    
+            (1, -1),  
+            (-1, 1),   
+            (-1, -1)   
+        ]
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            
+            while board.is_inside(nx, ny):
+                piece = board.get_piece(nx, ny)
+
+                if piece is None:
+                    moves.append((nx, ny))
+
+                elif piece.color != self.color:
+                    moves.append((nx, ny))
+                    break  
+                else:
+                    break   
+                nx += dx
+                ny += dy
+                
+        return moves
